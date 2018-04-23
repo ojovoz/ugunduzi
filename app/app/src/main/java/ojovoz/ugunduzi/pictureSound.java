62d19108dacd926b03a300999c2a46a6eee29f2a
ojovoz <cubo23@yahoo.com>
2018-04-23 12:01:24 +0200

    Picture + sound (in progress)
    Picture + sound (in progress)

--- app/app/src/main/java/ojovoz/ugunduzi/pictureSound.java
+++ app/app/src/main/java/ojovoz/ugunduzi/pictureSound.java
@@ -2,19 +2,43 @@ package ojovoz.ugunduzi;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by Eugenio on 19/04/2018.
  */
 public class pictureSound extends AppCompatActivity {
+
+    private int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+
+    String photoFile;
+    String prevPhotoFile;
+    boolean photoDone;
+
+    private audioRecorder soundRecorder;
+    private Boolean recording;
+    private boolean recordingDone;
 
     public String user;
     public String userPass;
@@ -33,6 +57,8 @@ public class pictureSound extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture_sound);
+
+        createDir(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath() + File.separator + getString(R.string.app_name) + File.separator);
 
         crop1 = new oCrop(this);
         crop2 = new oCrop(this);
@@ -89,6 +115,10 @@ public class pictureSound extends AppCompatActivity {
             }
         }
         tt.setText(title);
+
+        photoDone=false;
+        recording = false;
+        recordingDone = false;
     }
 
     @Override
@@ -201,16 +231,190 @@ public class pictureSound extends AppCompatActivity {
 
     }
 
-    public void startCamera(View v){
-
+    public void createDir(String dir) {
+        File folder = new File(dir);
+        if (!folder.exists()) {
+            folder.mkdirs();
+        }
     }
 
-    public void showRecorder(View v){
+    public void startCamera(View v){
+        showCamera();
+    }
 
+    private void showCamera(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            File filename = null;
+            try {
+                filename = createImageFile();
+            } catch (IOException ex) {
+
+            }
+            if (filename != null) {
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(filename));
+                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String dataPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath() + File.separator + getString(R.string.app_name) + File.separator;
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File image = new File(dataPath + "i" + timeStamp + ".jpg");
+
+        photoFile = image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            Bitmap thumb = scaleBitmap(photoFile);
+            if (thumb != null) {
+                ImageView thumbnail = (ImageView) this.findViewById(R.id.thumbnail);
+                thumbnail.setImageBitmap(thumb);
+                thumbnail.invalidate();
+            }
+
+            photoDone = true;
+            bChanges = true;
+            prevPhotoFile = photoFile;
+
+            if (recordingDone) {
+                Button saveButton = (Button) findViewById(R.id.saveButton);
+                saveButton.setVisibility(View.VISIBLE);
+
+            }
+
+        } else if(!prevPhotoFile.isEmpty()) {
+            photoFile = prevPhotoFile;
+        }
+    }
+
+    public Bitmap scaleBitmap(String path){
+        Bitmap ret=null;
+        final int IMAGE_MAX_SIZE = 1200000;
+        try{
+            InputStream in = null;
+            in = new FileInputStream(path);
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(in, null, options);
+            in.close();
+
+            int scale = 1;
+            while ((options.outWidth * options.outHeight) * (1 / Math.pow(scale, 2)) > IMAGE_MAX_SIZE) {
+                scale++;
+            }
+
+            in = new FileInputStream(path);
+
+            if (scale > 1) {
+                scale--;
+                options = new BitmapFactory.Options();
+                options.inSampleSize = scale;
+                ret = BitmapFactory.decodeStream(in, null, options);
+                in.close();
+
+                int height = ret.getHeight();
+                int width = ret.getWidth();
+
+                double y = Math.sqrt(IMAGE_MAX_SIZE / (((double) width) / height));
+                double x = (y / height) * width;
+
+                Bitmap scaledBitmap = Bitmap.createScaledBitmap(ret, (int) x, (int) y, true);
+
+                try
+                {
+                    FileOutputStream out = new FileOutputStream(path);
+                    scaledBitmap.compress(Bitmap.CompressFormat.JPEG, 80, out);
+                    out.close();
+                } catch (Exception e) {
+
+                }
+
+                ret.recycle();
+                ret = scaledBitmap;
+                return ret;
+
+            } else {
+                ret = BitmapFactory.decodeStream(in);
+                in.close();
+                return ret;
+            }
+
+
+        } catch (IOException e){
+
+        }
+        return ret;
+    }
+
+    public void recordSound(View v){
+        if (!recording) {
+            soundRecorder = new audioRecorder();
+            if (!soundRecorder.getFilename().equals("null")) {
+                deleteFile(soundRecorder.getFilename(), false);
+            }
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            soundRecorder.modifyPath(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath() + File.separator + getString(R.string.app_name) + File.separator + "s" + timeStamp + ".amr");
+            try {
+                Button bRec = (Button) findViewById(R.id.soundButton);
+                bRec.setBackgroundResource(R.drawable.button_background_rec);
+                bRec.setText(R.string.soundButtonRecordingLabel);
+                TextView textSoundRecorded = (TextView) findViewById(R.id.textSoundRecorded);
+                textSoundRecorded.setText(R.string.emptyString);
+                Button bSave = (Button) findViewById(R.id.saveButton);
+                bSave.setVisibility(View.GONE);
+                soundRecorder.start();
+                recording = true;
+            } catch (IOException e) {
+                //
+            }
+        } else {
+            if (soundRecorder != null) {
+                try {
+                    Button bRec = (Button) findViewById(R.id.soundButton);
+                    bRec.setText(R.string.soundButtonLabelAgain);
+                    bRec.setBackgroundResource(R.drawable.button_background);
+                    soundRecorder.stop();
+                    recording = false;
+                    recordingDone = true;
+                    bChanges = true;
+                    if (photoDone) {
+                        Button saveButton = (Button) findViewById(R.id.saveButton);
+                        saveButton.setVisibility(View.VISIBLE);
+                    }
+                    TextView textSoundRecorded = (TextView) findViewById(R.id.textSoundRecorded);
+                    textSoundRecorded.setText(R.string.soundRecordedMessage);
+                } catch (IOException e) {
+                    //
+                }
+            }
+        }
     }
 
     public void saveMessage(View v){
 
+    }
+
+    private void deleteFile(String f, boolean isImage) {
+        File fileX = new File(f);
+        long imgFileDate = fileX.lastModified();
+        fileX.delete();
+        if (isImage) {
+            String defaultGalleryPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).getAbsolutePath() + File.separator + "Camera";
+            File imgs = new File(defaultGalleryPath);
+            File imgsArray[] = imgs.listFiles();
+            for (int i = 0; i < imgsArray.length; i++) {
+                if (Math.abs(imgsArray[i].lastModified() - imgFileDate) <= 3000) {
+                    imgsArray[i].delete();
+                    break;
+                }
+            }
+        }
     }
 
 }
