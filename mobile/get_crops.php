61f6d592c729464cc861c099cc17085a8916d9bb
ojovoz <cubo23@yahoo.com>
2018-12-11 15:14:23 -0600

    English version (in progress)
    English version (in progress)

--- mobile/get_crops.php
+++ mobile/get_crops.php
@@ -2,8 +2,18 @@
 include_once "./../includes/init_database.php";
 $dbh = initDB();
 
+if(isset($_GET['lang'])){
+	if($_GET['lang']=="en"){
+		$name_field="crop_name_english";
+	} else {
+		$name_field="crop_name";
+	}
+} else {
+	$name_field="crop_name";
+}
+
 $df = fopen("php://output", 'w');
-$query="SELECT crop_id, crop_name, crop_variety FROM crop ORDER BY crop_name, crop_variety";
+$query="SELECT crop_id, ".$name_field.", crop_variety FROM crop ORDER BY ".$name_field.", crop_variety";
 $result = mysqli_query($dbh,$query);
 while($row = mysqli_fetch_array($result,MYSQL_NUM)){
 	fputcsv($df, $row);
--- mobile/get_units.php
+++ mobile/get_units.php
@@ -2,8 +2,18 @@
 include_once "./../includes/init_database.php";
 $dbh = initDB();
 
+if(isset($_GET['lang'])){
+	if($_GET['lang']=="en"){
+		$name_field="units_name_english";
+	} else {
+		$name_field="units_name";
+	}
+} else {
+	$name_field="units_name";
+}
+
 $df = fopen("php://output", 'w');
-$query="SELECT units_id, units_name, units_type FROM units ORDER BY units_name";
+$query="SELECT units_id, ".$name_field.", units_type FROM units ORDER BY ".$name_field;
 $result = mysqli_query($dbh,$query);
 while($row = mysqli_fetch_array($result,MYSQL_NUM)){
 	fputcsv($df, $row);
